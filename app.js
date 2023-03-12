8b38176302730dea0b9ea05566f820ce924f599d
Choibalsan <choiboosmile@gmail.com>
2023-03-12 17:55:41 +0800

    Orlogo zarlagaas tusviig boddog bolgono

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var uiController = (function () {
     inputDescription: '.add__description',
     inputValue: '.add__value',
     addButton: '.add__btn',
+    incomeList: '.income__list',
+    expenseList: '.expenses__list',
   };
 
   return {
@@ -14,6 +16,37 @@ var uiController = (function () {
         value: document.querySelector(DOMStrings.inputValue).value,
       };
     },
+    addListItem: function (item, type) {
+      // Орлого зарлагад зориулсан HTML бэлтгэнэ
+      var html, listDom;
+      if (type === 'inc') {
+        html =
+          '<div class="item clearfix" id="inc-%%1%%"><div class="item__description">%%desc%%</div><div class="right clearfix"><div class="item__value">+ %%val%%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div>';
+        listDom = DOMStrings.incomeList;
+      } else {
+        html =
+          '<div class="item clearfix" id="exp-%%1%%"><div class="item__description">%%desc%%</div><div class="right clearfix"><div class="item__value">- %%val%%</div><div class="item__percentage">10%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+        listDom = DOMStrings.expenseList;
+      }
+      html = html.replace('%%1%%', item.id);
+      html = html.replace('%%desc%%', item.desc);
+      html = html.replace('%%val%%', item.val);
+
+      // Tохирох хэсэгт гаргана
+      document.querySelector(listDom).insertAdjacentHTML('beforeend', html);
+    },
+
+    clearField: function () {
+      // Node elements
+      var fields = document.querySelectorAll(
+        DOMStrings.inputDescription + ', ' + DOMStrings.inputValue
+      );
+
+      var fieldsArr = Array.prototype.slice.call(fields);
+      fieldsArr.forEach((el) => (el.value = ''));
+      fieldsArr[0].focus();
+    },
+
     getDomStrings: function () {
       return DOMStrings;
     },
@@ -24,13 +57,13 @@ var fnController = (function () {
   var Income = function (id, desc, val) {
     this.id = id;
     this.desc = desc;
-    this.val = val;
+    this.val = parseInt(val);
   };
 
   var Expense = function (id, desc, val) {
     this.id = id;
     this.desc = desc;
-    this.val = val;
+    this.val = parseInt(val);
   };
 
   var data = {
@@ -42,9 +75,41 @@ var fnController = (function () {
       inc: 0,
       exp: 0,
     },
+    budget: 0,
+    percentage: 0,
+  };
+
+  var calculateTotal = function (type) {
+    var sum = 0;
+    data.items[type].forEach((el) => (sum += el.val));
+    data.total[type] = sum;
+    console.log(sum);
   };
 
   return {
+    calculateBudget: function () {
+      // Нийт орлогуудыг тооцоолно
+      calculateTotal('inc');
+
+      // Нийт зарлагуудыг тооцоолно
+      calculateTotal('exp');
+
+      // Төсвтийг шинээр тооцоолно
+      data.budget = data.total.inc - data.total.exp;
+
+      // Нийт зарлагын эзлэх хувийг олно
+      data.percentage = Math.round((data.total.exp / data.total.inc) * 100);
+    },
+
+    getBudget: function () {
+      return {
+        totalInc: data.total.inc,
+        totalExp: data.total.exp,
+        budget: data.budget,
+        percentage: data.percentage,
+      };
+    },
+
     saveItems: function (type, desc, val) {
       var item, id;
 
@@ -57,6 +122,7 @@ var fnController = (function () {
         item = new Expense(id, desc, val);
       }
       data.items[type].push(item);
+      return item;
     },
     seeData: function () {
       return data;
@@ -69,11 +135,27 @@ var appController = (function (uiCtrl, fnCtrl) {
     // 1. Дэлгэцнээс утгуудыг авна
     var input = uiCtrl.getInput();
 
-    // 2. Санхүүгийн контроллерт дамжуулж тэнд хадгална
-    fnController.saveItems(input.type, input.description, input.value);
-    // 3. Дэлгэцэнд тохирох хэсэгт гаргана
-    // 4. Төсвийг тооцоолно
-    // 5. Эцсийн үлдэгдлийг дэлгэцэнд үзүүлнэ
+    if (input.description !== '' && input.value !== '') {
+      // 2. Санхүүгийн контроллерт дамжуулж тэнд хадгална
+      var item = fnController.saveItems(
+        input.type,
+        input.description,
+        input.value
+      );
+
+      // 3. Дэлгэцэнд тохирох хэсэгт гаргана
+      uiController.addListItem(item, input.type);
+      uiController.clearField();
+
+      // 4. Төсвийг тооцоолно
+      fnController.calculateBudget();
+
+      // 5. Эцсийн үлдэгдлийг тооцоолно
+      var budget = fnController.getBudget();
+
+      // 6. Дэлгэцэнд үзүүлнэ
+      console.log(budget);
+    }
   };
 
   var setupEventListeners = function () {
@@ -100,5 +182,4 @@ var appController = (function (uiCtrl, fnCtrl) {
     },
   };
 })(uiController, fnController);
-
 appController.init();
